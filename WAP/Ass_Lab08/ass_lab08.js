860ae7279239d8ea5e5afadec2cc13c1a39aad64
Kareem AbuRijela <39139830+KareemAbuRejila@users.noreply.github.com>
2021-04-30 01:20:22 -0500

    Edit LikedList Ass_lab08

--- WAP/Ass_Lab08/ass_lab08.js
+++ WAP/Ass_Lab08/ass_lab08.js
@@ -3,13 +3,16 @@ function Student(fName, lName) {
     this.lastName = lName;
     this.grades = [];
     // this.getFullName = () => { return this.firstName + ' ' + this.lastName }
-    this.inputNewGrade = (newGrade) => { this.grades.push(newGrade) }
-    this.computeAverageGrade = () => {
-        return this.grades
-            .reduce((acc, c) => acc + c, 0) / this.grades.length;
+    this.inputNewGrade = (newGrade) => {
+        this.grades.push(newGrade);
     };
+    this.computeAverageGrade = () => {
+        return this.grades.reduce((acc, c) => acc + c, 0) / this.grades.length;
+    };
+}
+Student.prototype.getFullName = () => {
+    return this.firstName + " " + this.lastName;
 };
-Student.prototype.getFullName = () => { return this.firstName + ' ' + this.lastName };
 // Student.prototype.inputNewGrade = (newGrade) => { this.grades.push(newGrade) }
 // Student.prototype.computeAverageGrade = () => {
 //     return this.grades
@@ -36,32 +39,33 @@ students[2].inputNewGrade(59);
 
 for (let std in students) {
     console.log(students[std].getFullName());
-};
-console.log('------------------------------------');
+}
+console.log("------------------------------------");
 
 function printAverageForAllSudent() {
     for (let std in students) {
-        console.log(students[std].getFullName() + ' : ' + students[std].computeAverageGrade());
-    };
-};
+        console.log(
+            students[std].getFullName() + " : " + students[std].computeAverageGrade()
+        );
+    }
+}
 printAverageForAllSudent();
 //Implentation Student by Object Literal;
 StudentLit = {
-    firstName: '',
-    lastName: '',
+    firstName: "",
+    lastName: "",
     grades: [],
     inputNewGrade(newGrade) {
         this.grades.push(newGrade);
     },
     computeAverageGrade() {
-        return this.grades
-            .reduce((acc, c) => acc + c, 0) / this.grades.length;
+        return this.grades.reduce((acc, c) => acc + c, 0) / this.grades.length;
     },
-}
+};
 
 let studentLit1 = Object.create(StudentLit);
 studentLit1.firstName = "Kareem 1";
-studentLit1.lastName = 'AbuRjeila 1';
+studentLit1.lastName = "AbuRjeila 1";
 studentLit1.inputNewGrade(66);
 studentLit1.inputNewGrade(76);
 studentLit1.inputNewGrade(86);
@@ -69,63 +73,95 @@ studentLit1.inputNewGrade(96);
 
 let studentLit2 = Object.create(StudentLit);
 studentLit2.firstName = "Kareem 2";
-studentLit2.lastName = 'AbuRjeila 2';
+studentLit2.lastName = "AbuRjeila 2";
 studentLit2.inputNewGrade(66);
 studentLit2.inputNewGrade(76);
 studentLit2.inputNewGrade(86);
 studentLit2.inputNewGrade(96);
 let studentLit3 = Object.create(StudentLit);
 studentLit3.firstName = "Kareem 3";
-studentLit3.lastName = 'AbuRjeila 3';
+studentLit3.lastName = "AbuRjeila 3";
 studentLit3.inputNewGrade(66);
 studentLit3.inputNewGrade(76);
 studentLit3.inputNewGrade(86);
 studentLit3.inputNewGrade(96);
 
-let studentsLit = [
-    studentLit1, studentLit2, studentLit3
-];
-let avg = students.map(a => a.computeAverageGrade()).reduce((acc, c) => (acc + c), 0) / studentsLit.length;
+let studentsLit = [studentLit1, studentLit2, studentLit3];
+let avg =
+    students.map((a) => a.computeAverageGrade()).reduce((acc, c) => acc + c, 0) /
+    studentsLit.length;
 console.log(avg);
 
 //Implentation for LinkedList By Object literal;
-linkedlist = {
-    values: [],
-    add: function(a) {
-        this.values.push(a);
-    },
-    remove: function(a) {
-        this.values.pop(a);
-    },
-    print: function() {
-        console.log(this.values);
+console.log('------------------------ LinkedList ----------------------')
+class mNode {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+class mLinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    add(element) {
+        let node = new mNode(element);
+        let current;
+
+        if (this.head == null) this.head = node;
+        else {
+            current = this.head;
+            while (current.next) current = current.next;
+            current.next = node;
+        }
+        this.size++;
+    }
+    remove(element) {
+        let current = this.head;
+        let prev = null;
+
+        while (current != null) {
+            if (current.element === element) {
+                if (prev == null) this.head = current.next;
+                else prev.next = current.element;
+                this.size--;
+                return current.element;
+            }
+            prev = current;
+            current = current.next;
+        }
+        return -1;
+    }
+    insertAt(element, location) {}
+    reomveFrom(element, location) {}
+    isEmpty() { return this.size == 0; }
+    sizeOfList() { return this.size; }
+    printList() {
+        let current = this.head;
+        let str = '';
+        while (current) {
+            str += current.data + ' ,';
+            current = current.next;
+        }
+        console.log(str)
     }
 }
 
-linkedlist.add(1);
-linkedlist.add(2);
-linkedlist.add(3);
-linkedlist.print();
-linkedlist.remove(2);
-linkedlist.print();
 
-//Implentation for LinkedList By Constractor Funcation;
-function LinkedList() {
-    this.values = [];
-    this.add = function(a) {
-        this.values.push(a);
-    }
-    this.remove = function(a) {
-        this.values.pop(a);
-    }
-    this.print = function() {
-        console.log(this.values);
-    }
-}
-let linkedlist = new LinkedList();
-linkedlist.add(1);
-linkedlist.add(2);
-linkedlist.add(3);
-linkedlist.print();
-linkedlist.remove(2);
-linkedlist.print();
+let ll = new mLinkedList();
+
+console.log(ll.isEmpty());
+ll.add(10);
+ll.printList(ll);
+console.log(ll.sizeOfList());
+ll.add(20);
+ll.add(30);
+ll.add(40);
+ll.add(50);
+ll.printList(ll);
+console.log("is element removed ?" + ll.remove(50));
+ll.printList();
+console.log("is List Empty ? " + ll.isEmpty());
+ll.printList();
