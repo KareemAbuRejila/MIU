cba4ee09542c3110c89a4431f23e71a81b008c64
KareemAbuRejila <kareemaburejila@gmail.com>
2021-05-28 15:40:28 -0500

    MWA jobSearch remove node_modules

--- MWA/JobSearch/app.js
+++ MWA/JobSearch/app.js
@@ -9,11 +9,13 @@ const bodyParser=require('body-parser');
 app.set('port',5996);
 
 app.use(express.static(path.join(__dirname,'public')));
+app.use('/node_modules',express.static(path.join(__dirname,'node_modules')));
 // app.use(express.static({}))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/api',router);
+
 
 const server=app.listen(app.get('port'),()=>{
     const port=server.address().port;
